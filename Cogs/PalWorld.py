00dc2f81e74adfb8dd475d20574c3b6d282261aa
gcus-nm <gcusnm12@gmail.com>
2024-01-30 05:42:52 +0900

    体裁整え + 停止を監視できるかテスト

--- Cogs/PalWorld.py
+++ Cogs/PalWorld.py
@@ -31,7 +31,7 @@ class PalCog(commands.Cog, group_name='pal'):
     async def stop(self, ctx:commands.Context):
         print("PalWorldサーバーを停止します。")
         await ctx.send("PalWorldサーバーを停止します。")
-        await self.send_rcon_command("stop")
+        await self.send_rcon_command("DoExit")
 
     @pal.command(name="cmd", description="PalWorldサーバーでコマンドを使用します。")
     async def cmd(self, ctx:commands.Context, *, command:str):
@@ -43,10 +43,12 @@ class PalCog(commands.Cog, group_name='pal'):
 
     async def wait_pal_server_wakeup(self, ctx:commands.Context):
         await ctx.send("PalWorldサーバーを起動します。")
+        print(f"接続情報:　IP: {os.getenv('PALWORLD_SERVER_IP_ADDRESS')}, Port: {int(os.getenv('PALWORLD_RCON_PORT'))}")
+        # 起動まで待つ
         while True:
             try:
                 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                    s.settimeout(1)
+                    s.settimeout(3)
                     # RCONポートに接続できたら起動完了
                     s.connect((os.getenv("PALWORLD_SERVER_IP_ADDRESS"), int(os.getenv("PALWORLD_RCON_PORT"))))
                     print("PALWORLD RCONポート接続成功")
@@ -54,9 +56,22 @@ class PalCog(commands.Cog, group_name='pal'):
                     break
             except:
                 print("PALWORLD RCONポート接続失敗")
-                await asyncio.sleep(1)
+                await asyncio.sleep(3)
                 print("再接続開始...")
                 pass
+        pass
+
+        # 停止まで待つ
+        while True:
+            try:
+                with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+                    s.settimeout(5)
+                    # RCONポートに接続できたら起動完了
+                    s.connect((os.getenv("PALWORLD_SERVER_IP_ADDRESS"), int(os.getenv("PALWORLD_RCON_PORT"))))
+                    pass
+            except:
+                await ctx.send("PalWorldサーバーが停止しました。")
+                break
         pass
 
     async def send_rcon_command(self, command:str, ctx:commands.Context=None):
